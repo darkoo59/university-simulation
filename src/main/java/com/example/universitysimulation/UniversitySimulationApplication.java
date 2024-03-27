c9ef497513a58909ca82bf9b4b01e581edcc88b8
darkoo59 <darko.selakovic11@gmail.com>
2024-03-27 12:26:27 +0100

    Initial commit

---
+++ src/main/java/com/example/universitysimulation/UniversitySimulationApplication.java
@@ -0,0 +1,13 @@
+package com.example.universitysimulation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversitySimulationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversitySimulationApplication.class, args);
+	}
+
+}
